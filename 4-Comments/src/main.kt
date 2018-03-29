58e05aa648735896ddbfe669f4804e4de643ec16
Katie Snell <katie.snell@student.manchester.ac.uk>
2018-03-29 15:48:24 +0100

    Completed 4. Comments

---
+++ 4-Comments/src/main.kt
@@ -0,0 +1,21 @@
+fun main(args : ArrayList<String>) {
+    // This is a comment. It is not executed
+
+    // This is also a comment.
+    // Over multiple lines.
+
+    /* This is a comment
+       over
+       many
+       lines...
+     */
+
+    /* This is a comment
+
+    /* And inside it this is also a comment
+
+     */
+     * Back to the first comment
+     * Another line
+     */
+}
